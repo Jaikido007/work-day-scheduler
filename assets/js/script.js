3bbfffe468a1eb83c610c887b991136a31d5d58d
jaikido007 <jaymullen@gmail.com>
2023-01-30 11:02:58 +0000

    Setting up the javascript file with todo comments

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// TODO Display current day at the top of the calendar
+
+// TODO Create timeblocks for standard business hours
+
+// TODO Color-code each timeblock based on past, present, or future
+
+// TODO Save event to local storage when save button is clicked
+
+// TODO Retrieve events from local storage
