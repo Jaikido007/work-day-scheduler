a160557d25e24c042b66477a75b24437a11aa9c8
jaikido007 <jaymullen@gmail.com>
2023-01-30 13:42:29 +0000

    Styling all the elements and adding a typewriter feature to the header h1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,79 +1,79 @@
 $(document).ready(function () {
-// Display current day at the top of the calendar
-let currentDay = moment().format("dddd, MMMM Do YYYY");
-$("#currentDay").text(currentDay);
+	// Display current day at the top of the calendar
+	let currentDay = moment().format("dddd, MMMM Do YYYY");
+	$("#currentDay").text(currentDay);
 
-// Create timeblocks for standard business hours
-let businessHours = [
-	"9am",
-	"10am",
-	"11am",
-	"12pm",
-	"1pm",
-	"2pm",
-	"3pm",
-	"4pm",
-	"5pm",
-];
+	// Create timeblocks for standard business hours
+	let businessHours = [
+		"9am",
+		"10am",
+		"11am",
+		"12pm",
+		"1pm",
+		"2pm",
+		"3pm",
+		"4pm",
+		"5pm",
+	];
 
-for (let i = 0; i < businessHours.length; i++) {
-	let hour = businessHours[i];
-	let hourDiv = $("<div>").addClass("timeblock");
-	let hourLabel = $("<label>").text(hour).addClass("time-of-day");
-	let input = $("<input>").addClass("description");
-	let saveBtn = $("<button>")
-		.addClass("saveBtn")
-		.html("<i class='fas fa-save'></i>");
-	let clearBtn = $("<button>")
-		.addClass("clearBtn")
-		.html("<i class='fas fa-trash'></i>");
-	hourDiv.append(hourLabel, input, saveBtn, clearBtn);
-	$(".container").append(hourDiv);
-}
-
-// Color-code each timeblock based on past, present, or future
-let currentHour = moment().format("ha");
-$(".timeblock").each(function () {
-	let hour = $(this).find("label").text();
-	let hourMoment = moment(hour, "ha");
-	if (hourMoment.isBefore(moment(currentHour, "ha"))) {
-		$(this).addClass("past");
-	} else if (hourMoment.isSame(moment(currentHour, "ha"))) {
-		$(this).addClass("present");
-	} else if (hourMoment.isSame(moment(currentHour, "ha"))) {
-		$(this).addClass("present");
-	} else {
-		$(this).addClass("future");
+	for (let i = 0; i < businessHours.length; i++) {
+		let hour = businessHours[i];
+		let hourDiv = $("<div>").addClass("timeblock");
+		let hourLabel = $("<label>").text(hour).addClass("time-of-day");
+		let input = $("<input>").addClass("description");
+		let saveBtn = $("<button>")
+			.addClass("saveBtn")
+			.html("<i class='fas fa-save'></i>");
+		let clearBtn = $("<button>")
+			.addClass("clearBtn")
+			.html("<i class='fas fa-trash'></i>");
+		hourDiv.append(hourLabel, input, saveBtn, clearBtn);
+		$(".container").append(hourDiv);
 	}
-});
 
-// Save event to local storage when save button is clicked
-$(".saveBtn").on("click", function () {
-	let hour = $(this).parent().find("label").text();
-	let event = $(this).parent().find(".description").val();
-	localStorage.setItem(hour, event);
-	console.log(localStorage); // works: storing inputted values in LS
-});
+	// Color-code each timeblock based on past, present, or future
+	let currentHour = moment().format("ha");
+	$(".timeblock").each(function () {
+		let hour = $(this).find("label").text();
+		let hourMoment = moment(hour, "ha");
+		if (hourMoment.isBefore(moment(currentHour, "ha"))) {
+			$(this).addClass("past");
+		} else if (hourMoment.isSame(moment(currentHour, "ha"))) {
+			$(this).addClass("present");
+		} else if (hourMoment.isSame(moment(currentHour, "ha"))) {
+			$(this).addClass("present");
+		} else {
+			$(this).addClass("future");
+		}
+	});
 
-// Retrieve events from local storage
+	// Save event to local storage when save button is clicked
+	$(".saveBtn").on("click", function () {
+		let hour = $(this).parent().find("label").text();
+		let event = $(this).parent().find(".description").val();
+		localStorage.setItem(hour, event);
+		console.log(localStorage); // works: storing inputted values in LS
+	});
 
-$(".description").each(function () {
-	let hour = $(this).closest(".timeblock").find("label").text();
-	let event = localStorage.getItem(hour);
-	$(this).val(event);
-});
+	// Retrieve events from local storage
 
-// Retrieve events from local storage and display in timeblocks
-for (let i = 0; i < businessHours.length; i++) {
-	let hour = businessHours[i];
-	let event = localStorage.getItem(hour);
-	$("label:contains(" + hour + ")")
-		.parent()
-		.find(".description")
-		.val(event);
-}
+	$(".description").each(function () {
+		let hour = $(this).closest(".timeblock").find("label").text();
+		let event = localStorage.getItem(hour);
+		$(this).val(event);
+	});
 
-	// Clear event from local storage when button is clicked 
+	// Retrieve events from local storage and display in timeblocks
+	for (let i = 0; i < businessHours.length; i++) {
+		let hour = businessHours[i];
+		let event = localStorage.getItem(hour);
+		$("label:contains(" + hour + ")")
+			.parent()
+			.find(".description")
+			.val(event);
+	}
+
+	// Clear event from local storage when button is clicked
 	$(".clearBtn").on("click", function () {
 		let hour = $(this).parent().find("label").text();
 		localStorage.removeItem(hour);
